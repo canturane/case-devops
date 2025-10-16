be2ba612b42bdda3fd93bba4564ecfcc471bc83b
Salih Can Turan <117317504+canturane@users.noreply.github.com>
2025-10-16 11:16:08 +0300

    Remove checkmark from installation message

--- scripts/install_kind_cli.sh
+++ scripts/install_kind_cli.sh
@@ -10,4 +10,4 @@ VER="v0.23.0"
 curl -L -o kind "https://github.com/kubernetes-sigs/kind/releases/download/${VER}/kind-linux-${KARCH}"
 sudo install -o root -g root -m 0755 kind /usr/local/bin/kind
 rm -f kind
-echo "✅ kind $(kind --version) installed."
+echo " kind $(kind --version) installed."
